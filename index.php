3abded9692d160b7af60c0484907494f6903ac83
ahugla <ahugla@vmware.com>
2018-02-01 22:20:58 +0100

    tous les niveaux de log dans syslog
    0  LOG_EMERG
    1  LOG_ALERT
    2  LOG_CRIT
    3  LOG_ERR
    4  LOG_WARNING
    5  LOG_NOTICE
    6  LOG_INFO
    7  LOG_DEBUG

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Exemple</title>
+<title>PHP LOG TEST</title>
 </head>
 <body>
 
 <?php
-	echo "Bonjour, je suis un Script PHP !         ******         ";
+    echo "Bonjour, je suis un Script PHP !         ******         ";
     echo "fichier  : index.html         *******      ";
 
     echo "Log PHP via error_log        *******       ";
     Error_log("********** from error_log----------");
 
     echo "Log PHP via syslog       *********       FIN";
-    syslog("********** from syslog----------");
+    syslog(0,"********** from syslog 0----------");    # LOG_EMERG
+    syslog(1,"********** from syslog 1----------");    # LOG_ALERT
+    syslog(2,"********** from syslog 2----------");    # LOG_CRIT
+    syslog(3,"********** from syslog 3----------");    # LOG_ERR
+    syslog(4,"********** from syslog 4----------");    # LOG_WARNING
+    syslog(5,"********** from syslog 5----------");    # LOG_NOTICE
+    syslog(6,"********** from syslog 6----------");    # LOG_INFO
+    syslog(7,"********** from syslog 7----------");    # LOG_DEBUG
+
 ?>
 
 </body>
